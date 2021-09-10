c2f07ae36d1bd64bed98162a7f958887a3a7f6a6
Cristiananc <cristiananc@hotmail.com>
2021-09-09 23:34:47 -0300

    adding gamma elicitator r code

---
+++ Códigos/gamma_elicitator.r
@@ -0,0 +1,41 @@
+#### Elicit a gamma distribution such that Pr(X > S) = p
+### Note that Pr(1/sqrt(tau) > S) = p can be written as
+#### Pr(tau < 1/S^2) = p
+#### here, tau ~ Gamma(shape = a, scale = b)
+
+loss <- function(par, S, p){
+  a <- exp(par[1])
+  b <- exp(par[2])
+  abs(
+    pgamma(q = 1/S^2, shape = a, scale = b) - p
+  )
+}
+
+get_gamma_pars <- function(S, p){
+  opt <- optim(par = c(0, 0), fn = loss,
+               S = S, p = p)
+  out <- exp(opt$par)
+  return(out)
+}
+
+SS <- 1
+pp <- .1
+
+the.pars <- get_gamma_pars(S = SS, p = pp)
+
+curve(dgamma(x, shape = the.pars[1],
+             scale = the.pars[2]), 0, 10*SS,
+      ylab = "Density")
+
+precisions <- rgamma(n = 1E6,
+            shape = the.pars[1],
+            scale = the.pars[2]
+            )
+
+sds <- 1/sqrt(precisions)
+
+mean(sds > SS)
+
+hist(precisions)
+
+the.pars
